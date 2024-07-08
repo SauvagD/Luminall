c35c6f374815a5393e4dacef005460d448a6af49
SauvagD <ds.sauvage@gmail.com>
2024-07-08 23:55:34 +0200

    Social links

--- src/constant.ts
+++ src/constant.ts
@@ -6,8 +6,20 @@ import linkedin from "../public/icons/social-medias/linkedin.svg";
 export const VIDEO_FORMATS = ["mov", "mp4"];
 
 export const SOCIAL_MEDIAS = [
-  { name: "linkedin", href: "", icon: linkedin },
-  { name: "instagram", href: "", icon: insta },
-  { name: "tiktok", href: "", icon: tiktok },
-  { name: "twitter", href: "", icon: twitter },
+  {
+    name: "linkedin",
+    href: "https://www.linkedin.com/in/luminall-luminall-54bb81315/",
+    icon: linkedin,
+  },
+  {
+    name: "instagram",
+    href: "https://www.instagram.com/luminall.studio/",
+    icon: insta,
+  },
+  // {
+  //   name: "tiktok",
+  //   href: "https://www.tiktok.com/discover?lang=fr",
+  //   icon: tiktok,
+  // },
+  { name: "twitter", href: "https://x.com/Luminall_studio", icon: twitter },
 ];
